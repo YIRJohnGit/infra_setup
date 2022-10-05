89a412f7b3939310a5d982f9881fb2b1468ac491
YIRJohnGit <111234771+YIRJohnGit@users.noreply.github.com>
2022-10-05 07:24:33 +0530

    Create default_utility.sh

---
+++ default_utility.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y ssh
+sudo apt install -y openssh-server
+sudo apt install -y git # Install Git
