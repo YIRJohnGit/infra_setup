f0cf37f1ef0f7d9c02ee9f6f8ca751437964f409
YIRJohnGit <111234771+YIRJohnGit@users.noreply.github.com>
2022-10-09 11:08:52 +0530

    Update default_utility.sh

--- default_utility.sh
+++ default_utility.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates software-properties-common
 sudo apt install -y ssh
 sudo apt install -y openssh-server
-sudo apt install -y git # Install Git
 sudo apt install -y tree
 ssh-keygen
