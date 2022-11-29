fe683bd138ff80574d887ea3184e3810b3476a7e
YIRJohnGit <111234771+YIRJohnGit@users.noreply.github.com>
2022-11-29 19:53:28 +0530

    Update step1_k8s_all_nodes.sh

--- k8s/step1_k8s_all_nodes.sh
+++ k8s/step1_k8s_all_nodes.sh
@@ -8,7 +8,8 @@ sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release \
+    jq
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
