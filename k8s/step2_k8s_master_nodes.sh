c93523d85ceef81dd26406bf06e03e8b1dd08e3e
YIRJohnGit <111234771+YIRJohnGit@users.noreply.github.com>
2022-11-30 00:48:35 +0530

    Update step2_k8s_master_nodes.sh

--- k8s/step2_k8s_master_nodes.sh
+++ k8s/step2_k8s_master_nodes.sh
@@ -3,4 +3,4 @@
 sudo kubeadm init
 sudo mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/confi
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
