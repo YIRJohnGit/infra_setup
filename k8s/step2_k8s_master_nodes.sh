8bb203772ae6fd7207898efcb81424b951cb1743
YIRJohnGit <111234771+YIRJohnGit@users.noreply.github.com>
2022-12-01 01:06:49 +0530

    Update step2_k8s_master_nodes.sh

--- k8s/step2_k8s_master_nodes.sh
+++ k8s/step2_k8s_master_nodes.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 sudo kubeadm init
+# OR Collect the master  and run the INitIP Address
+export MASTER_IP=192.168.56.116
+sudo kubeadm init --apiserver-advertise-address=${MASTER_IP} --pod-network-cidr 10.10.0.0/16
+
+
 sudo mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
