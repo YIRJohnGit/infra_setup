c48773c66fb62adb9fbf770215da293f5eceb3fa
YIRJohnGit <111234771+YIRJohnGit@users.noreply.github.com>
2022-12-17 02:19:56 +0530

    Update jenkins_installation.sh

--- jenkins_installation.sh
+++ jenkins_installation.sh
@@ -3,6 +3,7 @@
 sudo curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 sudo apt update -y
 sudo apt install -y openjdk-8-jdk openjdk-8-jre
+
 sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update -y
