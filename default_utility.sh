b8df1a01f8007dd9a79e56ae8aa90159806af3f9
YIRJohnGit <111234771+YIRJohnGit@users.noreply.github.com>
2022-12-26 13:47:24 +0530

    Update default_utility.sh

--- default_utility.sh
+++ default_utility.sh
@@ -5,5 +5,6 @@ sudo apt install -y vim
 sudo apt install -y apt-transport-https ca-certificates software-properties-common
 sudo apt install -y ssh
 sudo apt install -y openssh-server
+sudo apt install -y net-tools
 sudo apt install -y tree
 ssh-keygen
