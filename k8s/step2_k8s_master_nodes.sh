7ed030db64049119021870d3bc67bca65045fa2d
YIRJohnGit <111234771+YIRJohnGit@users.noreply.github.com>
2022-12-29 10:03:18 +0530

    Update step2_k8s_master_nodes.sh

--- k8s/step2_k8s_master_nodes.sh
+++ k8s/step2_k8s_master_nodes.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-sudo kubeadm init
-# OR Collect the master  and run the INitIP Address
-export MASTER_IP=`hostname -I | awk '{print $2}'`
-sudo kubeadm init --apiserver-advertise-address=${MASTER_IP} --pod-network-cidr 10.10.0.0/16
+echo "Kube admin can be run on specific IP Address or current local IP Address also below is example"
+echo "=============================================================================================="
+echo "- to run the below for auto ip address for master node"
+echo "sudo kubeadm init"
+echo "\n\n\nOR Build the cluster on the Current IP Address as menstion below (Copy Paste)"
+echo "export MASTER_IP=`hostname -I | awk '{print $2}'`"
+echo "sudo kubeadm init --apiserver-advertise-address=${MASTER_IP} --pod-network-cidr 10.10.0.0/16"
 
 
 sudo mkdir -p $HOME/.kube
