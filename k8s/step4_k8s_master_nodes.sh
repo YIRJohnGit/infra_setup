5a62d57eead4ff3c26422e021d773b92916f9f2a
YIRJohnGit <111234771+YIRJohnGit@users.noreply.github.com>
2023-01-07 19:53:36 +0530

    Update step4_k8s_master_nodes.sh

--- k8s/step4_k8s_master_nodes.sh
+++ k8s/step4_k8s_master_nodes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 kubectl get nodes # Verify the status of nodes
-kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+sudo kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
