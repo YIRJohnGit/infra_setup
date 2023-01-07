019da13616bcfb50c3aeb6b5d5d2667ba5ce487a
YIRJohnGit <111234771+YIRJohnGit@users.noreply.github.com>
2023-01-08 03:38:12 +0530

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -6,4 +6,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu    $(lsb_release -cs) stable"
 sudo apt-get install -y docker-ce
 sudo apt-get update
+sudo docker ps
 sudo docker run hello-world
+sudo docker ps
+sudo docker run -it ubuntu bash
