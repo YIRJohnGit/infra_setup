37dcc65e4a4e27d1851238dc387b5da1a4962675
YIRJohnGit <111234771+YIRJohnGit@users.noreply.github.com>
2023-01-24 00:14:33 +0530

    Update step1_k8s_all_nodes.sh

--- k8s/step1_k8s_all_nodes.sh
+++ k8s/step1_k8s_all_nodes.sh
@@ -38,9 +38,10 @@ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pack
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 
-KUBE_VERSION=v1.25.4
-
+KUBE_VERSION=1.25.4-00
 sudo apt-get install -y kubelet="$KUBE_VERSION" kubeadm="$KUBE_VERSION" kubectl="$KUBE_VERSION"
+
 sudo apt-mark hold kubelet kubeadm kubectl
+echo "sudo apt-mark unhold kubelet kubeadm kubectl"
 sudo rm /etc/containerd/config.toml
 sudo systemctl restart containerd
