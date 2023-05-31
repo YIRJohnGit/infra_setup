50344acadb625d42d37363abcdda1826ffe79e47
Irudaya Raj John Y <111234771+YIRJohnGit@users.noreply.github.com>
2023-05-31 16:51:01 +0530

    Update step4_k8s_master_nodes.sh

--- k8s/step4_k8s_master_nodes.sh
+++ k8s/step4_k8s_master_nodes.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 kubectl get nodes # Verify the status of nodes
-wget https://raw.githubusercontent.com/projectcalico/calico/master/manifests/calico.yaml
-kubectl apply -f calico.yaml
-kubectl get pods -n kube-system
-
 wget https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
 kubectl apply -f  weave-daemonset-k8s.yaml
 kubectl get pods -n kube-system
+
+# kubectl get nodes # Verify the status of nodes
+# wget https://raw.githubusercontent.com/projectcalico/calico/master/manifests/calico.yaml
+# kubectl apply -f calico.yaml
+# kubectl get pods -n kube-system
