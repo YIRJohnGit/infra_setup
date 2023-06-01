6362cdf501effaa4dc0f2c75f6f44deaa66148ef
Irudaya Raj John Y <111234771+YIRJohnGit@users.noreply.github.com>
2023-06-01 12:15:54 +0530

    Update default_utility.sh

--- default_utility.sh
+++ default_utility.sh
@@ -3,8 +3,5 @@
 sudo apt update
 sudo apt install -y vim
 sudo apt install -y apt-transport-https ca-certificates software-properties-common
-sudo apt install -y ssh
-sudo apt install -y openssh-server
-sudo apt install -y net-tools
-sudo apt install -y tree
+sudo apt install -y ssh openssh-server net-tools tree iptables gnupg lsb-release jq
 ssh-keygen
