513bddd504a9fb10fa0d9da3482dd0594c67667c
Irudaya Raj John Y <111234771+YIRJohnGit@users.noreply.github.com>
2023-06-02 16:29:31 +0530

    Update step2_k8s_master_nodes.sh

--- k8s/step2_k8s_master_nodes.sh
+++ k8s/step2_k8s_master_nodes.sh
@@ -8,12 +8,22 @@ echo "\n\n\nOR Build the cluster on the Current IP Address as menstion below (Co
 echo ""
 echo "export MASTER_IP=`hostname -I | awk '{print $2}'`"
 echo "sudo kubeadm init --apiserver-advertise-address=\${MASTER_IP}"
+
+export MASTER_IP=`hostname -I | awk '{print $2}'`
+sudo kubeadm init --apiserver-advertise-address=${MASTER_IP}
+join_command=$(kubeadm token create --print-join-command)
+output_file="cluster_join_key.txt"
+echo "sudo $join_command" >> "$output_file"
+
 echo ""
 echo ""
 echo "Optional to add cidr range"
 echo "=========================="
 echo "sudo kubeadm init --apiserver-advertise-address=\${MASTER_IP} --pod-network-cidr 10.10.0.0/16"
 
-#sudo mkdir -p $HOME/.kube
-#sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-#sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo mkdir -p $HOME/.kube
+sudo cp $HOME/.kube/config $HOME/.kube/config_bak_$(date +"%Y%m%d_%H%M%S")
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
